b119d9f259931b166c0fe8be899d278463e1e4a7
TwilCynder <cyndercube@gmail.com>
2017-11-14 12:27:14 +0100

    Correction du bug des rubis

--- data/scripts/initial_game.lua
+++ data/scripts/initial_game.lua
@@ -16,6 +16,7 @@ function initial_game:initialize_new_savegame(game)
 
   game:set_max_life(12)
   game:set_life(game:get_max_life())
+  game:set_max_money(10)
   game:set_ability("lift", 1)
   game:set_ability("sword", 1)
 end
