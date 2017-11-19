bf8dff3472c52b265e6f0f39e250bec0ce666ff1
TwilCynder <cyndercube@gmail.com>
2017-11-19 22:36:43 +0100

    Màj du HUD
    Affichage de l'image de fond (reste à modifier les rubis)

---
+++ data/scripts/hud/background.lua
@@ -0,0 +1,13 @@
+local background_builder = {}
+
+local background_img = sol.surface.create(320,24)
+background_img:fill_color{245,245,220}
+
+function background_builder:new(game,config)
+  local background = {}
+  function background:on_draw(dst_surface)
+    background_img:draw(dst_surface,config.x,config.y)
+  end
+  
+  return background
+end
